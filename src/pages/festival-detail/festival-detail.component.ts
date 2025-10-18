d72e31b896f9384fde23e08a5c365f3449f49636
Simone <simonerizzanl@gmail.com>
2025-10-18 02:30:51 +0200

    feat(festivals): search and sort
    * feat(festivals): search
    
    * feat(festivals): sort
    
    * feat(festivals): search by city and department

--- src/pages/festival-detail/festival-detail.component.ts
+++ src/pages/festival-detail/festival-detail.component.ts
@@ -1,10 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { FestivalService } from '@/services/festival.services';
 import { FestivalCardComponent } from '@/components/festival-card/festival-card.component';
-import { Festival, FestivalResponse, FestivalKeys } from '@/models/festival.model';
+import {
+  Festival,
+  FestivalResponse,
+  FestivalKeys,
+  festivalKeyTranslation,
+} from '@/models/festival.model';
 
 @Component({
   selector: 'festival-detail',
@@ -15,23 +25,47 @@ import { Festival, FestivalResponse, FestivalKeys } from '@/models/festival.mode
     CommonModule,
     FestivalCardComponent,
     MatDividerModule,
-    MatPaginatorModule
-  ]
+    MatPaginatorModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
 })
-export class FestivalDetailComponent {  
+export class FestivalDetailComponent {
   festivals: Festival[] = [];
   isLoading: boolean = false;
   pageSize: number = 5;
   pageIndex: number = 1;
   itemsLength: number = 0;
   pageSizeOptions: number[] = [5, 10, 25, 100];
+  searchInput: FormControl<string | null> = new FormControl('', []);
+  orderBy: FormControl<keyof Festival | null> = new FormControl(
+    FestivalKeys.NOM_DU_FESTIVAL,
+    []
+  );
+  festivalKeys = this.mapFestivalKeysForSelction();
 
-  constructor(private festivalService: FestivalService) { }
-  
+  constructor(private festivalService: FestivalService) {}
+
+  mapFestivalKeysForSelction() {
+    return Object.values(FestivalKeys)
+      .map((key) => {
+        const translation = festivalKeyTranslation[key] ?? null;
+        return {
+          value: key,
+          valueView: translation,
+        };
+      })
+      .filter((option) => !!option.valueView);
+  }
+
   handlePageEvent(event: any) {
     const hasPageSizeChanged: boolean = this.pageSize != event.pageSize;
     const hasPageIndexChanged: boolean = this.pageIndex != event.pageIndex + 1;
-    
+
     if (hasPageSizeChanged || hasPageIndexChanged) {
       this.pageSize = event.pageSize;
       this.pageIndex = event.pageIndex + 1;
@@ -41,20 +75,37 @@ export class FestivalDetailComponent {
 
   getFestivals() {
     this.isLoading = true;
-    this.festivalService.getFestivals(this.pageIndex, this.pageSize, FestivalKeys.NOM_DU_FESTIVAL).subscribe({
-      next: (response: FestivalResponse) => {
-        this.festivals = response.results;
-        this.itemsLength = response.total_count;
-        this.isLoading = false;
-      },
-      error: (e: Error) => {
-        console.log(e);
-        this.isLoading = false;
-      },
-    });
+    this.festivalService
+      .getFestivals(
+        this.pageIndex,
+        this.pageSize,
+        this.orderBy.value,
+        this.searchInput.value
+      )
+      .subscribe({
+        next: (response: FestivalResponse) => {
+          this.festivals = response.results;
+          this.itemsLength = response.total_count;
+          this.isLoading = false;
+        },
+        error: (e: Error) => {
+          console.log(e);
+          this.isLoading = false;
+        },
+      });
+  }
+
+  handleSearchInput() {
+    this.searchInput.valueChanges.subscribe(() => this.getFestivals());
+  }
+
+  handleSort() {
+    this.orderBy.valueChanges.subscribe(() => this.getFestivals());
   }
 
   ngOnInit() {
     this.getFestivals();
+    this.handleSearchInput();
+    this.handleSort();
   }
 }
--- src/services/festival.services.ts
+++ src/services/festival.services.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Festival, FestivalResponse } from '@/models/festival.model';
+import { Festival, FestivalKeys, FestivalResponse } from '@/models/festival.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,14 +12,33 @@ export class FestivalService {
 
   constructor(private http: HttpClient) {}
 
-  getFestivals(pageIndex: number, pageSize: number, orderBy: keyof Festival): Observable<FestivalResponse> {
+  getFestivals(pageIndex: number, pageSize: number, orderBy?: keyof Festival | null, searchInput?: string | null): Observable<FestivalResponse> {
     const offset = (pageIndex - 1) * pageSize;
+    const whereClause = this.getWhereClause(searchInput);
+    const orderClause = Object.values(FestivalKeys).includes(orderBy as any) ? orderBy as string
+     : FestivalKeys.NOM_DU_FESTIVAL;
 
-    const params = new HttpParams()
-      .set('order_by', orderBy)
+    let params = new HttpParams()
+      .set('order_by', orderClause)
       .set('limit', pageSize.toString())
       .set('offset', offset.toString());
+      
+    if (whereClause) {
+      params = params.set('where', whereClause);
+
+    }
     
     return this.http.get<FestivalResponse>(this.apiUrl, { params });
+  }
+
+  getWhereClause(searchInput?: string | null): string | null{
+    if (!searchInput) {
+      return null;
+    }
+    
+    const safeInput = searchInput.trim().replace(/'/g, "''");
+    return `${FestivalKeys.NOM_DU_FESTIVAL} like '%${safeInput}%' OR ` +
+          `${FestivalKeys.COMMUNE_PRINCIPALE_DE_DEROULEMENT} like '%${safeInput}%' OR ` +
+          `${FestivalKeys.DEPARTEMENT_PRINCIPAL_DE_DEROULEMENT} like '%${safeInput}%'`;
   }
 }
